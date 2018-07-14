d6675e5d628e44840b4c342a84e8eddfcff1477b
Marcel O'Neil <marcel@marceloneil.com>
2018-07-14 16:28:07 -0400

    json

--- client/public/script.js
+++ client/public/script.js
@@ -23,6 +23,7 @@ $(document).ready(function() {
     $.ajax({
       type: "POST",
       url: "http://one.dabcoin.1lab.me:5000/mine",
+      dataType: "json",
       data: {
         'dab_data':JSON.stringify(finalArr),
         'address': window.localStorage.getItem('address')
