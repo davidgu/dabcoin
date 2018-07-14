ec1f9e1650826ea442689b51302c8b48eb5c87cb
David Gu <david.gu@hotmail.com>
2018-07-14 14:25:27 -0400

    fixed things

--- server/app.py
+++ server/app.py
@@ -136,7 +136,7 @@ def mine():
     response = {
         'message': 'New block created',
         'index': block['index'],
-        'transactions': block['transaction'],
+        'transactions': block['transactions'],
         'proof': block['proof'],
         'previous_hash': block['previous_hash'],
     }
